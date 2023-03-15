1ff6a474ba00ac96f0cfeea8ea924dc8fe313cc5
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-03-15 10:25:41 -0300

    métodos Array (calculo de mediana)

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,6 +1,5 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
-TestaBuscarPalavra();
 
 void TestaArray()
 {
@@ -50,4 +49,29 @@ void TestaBuscarPalavra()
         }
         
     }
+}
+
+Array amostra = Array.CreateInstance(typeof(double), 6);
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10, 3);
+amostra.SetValue(6.9, 4);
+amostra.SetValue(8, 5);
+
+TestaMediana(amostra);
+void TestaMediana(Array array)
+{
+    if((array == null) || (array.Length == 0)){
+        Console.WriteLine("Array para cálculo da mediana está vazio ou nulo.");
+    }
+
+    double[] numerosOrdenados = (double[])array.Clone();
+    Array.Sort(numerosOrdenados);
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0)
+        ? numerosOrdenados[meio]
+        : (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+    Console.WriteLine($"Com base na amostra a mediana é: {mediana}");
 }
