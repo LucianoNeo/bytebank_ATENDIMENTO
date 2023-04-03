346d5df40f6800da5a6e37dd5dc872820db46dd1
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-04-03 12:24:03 -0300

    utilizando DLL

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,5 +1,18 @@
 ﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+using bytebank_GeradorChavePix;
 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+Console.WriteLine();
+//new ByteBankAtendimento().AtendimentoCliente();
 
-new ByteBankAtendimento().AtendimentoCliente();
+var listaDeChaves = new List<string>(GeradorPix.GetChavesPix(10));
+
+Console.WriteLine("Chave unica");
+Console.WriteLine(GeradorPix.GetChavePix());
+Console.WriteLine();
+Console.WriteLine("Lista de chaves");
+
+foreach (var chave in listaDeChaves)
+{
+    Console.WriteLine(chave);
+}
